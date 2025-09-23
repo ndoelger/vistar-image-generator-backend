ea5aa2e3fdcf57839e917688db59a07d9d329f04
Nic Doelger <doelgern@gmail.com>
2025-09-23 12:48:42 -0400

    init

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, request, redirect
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+from dotenv import load_dotenv
+
+from openai import OpenAI
+
+import os, base64
+import requests
+
+
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+
+app = Flask(__name__)
+CORS(app)
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+upload_dir = os.path.join(os.getcwd(), "uploads")
+os.makedirs(upload_dir, exist_ok=True)
+
+
+@app.route("/", methods=["POST"])
+def upload_local():
+
+    boto3.setup_default_session(profile_name="default")
+    s3 = boto3.client("s3")
+
+    brand_book = request.files.get("brandBook")
+    assets_zip = request.files.get("assets")
+    copy = request.form.get("copy")
+
+    s3.upload_fileobj(
+        brand_book, "vistar-dc", f"2025/09/ai-innovation/{brand_book.filename}"
+    )
+
+    s3.upload_fileobj(
+        assets_zip, "vistar-dc", f"2025/09/ai-innovation/{assets_zip.filename}"
+    )
+
+    s3.put_object(
+        Body=copy,
+        Bucket="vistar-dc",
+        Key=f"2025/09/ai-innovation/copy.txt",
+    )
+
+    return redirect("/generate")
+    # brand_file_name = secure_filename(brand_book.filename)
+    # assets_zip_name = secure_filename(assets_zip.filename)
+
+    # brand_book.save(os.path.join(upload_dir, brand_file_name))
+    # assets_zip.save(os.path.join(upload_dir, assets_zip_name))
+
+    # copy_path = os.path.join(upload_dir, "copy.txt")
+
+    # with open(copy_path, "w") as t:
+    #     t.write(copy)
+
+
+@app.route("/generate")
+def openai_gen():
+    try:
+        boto3.setup_default_session(profile_name="default")
+        s3 = boto3.client("s3")
+
+        response = s3.get_object(
+            Bucket="vistar-dc", Key="2025/09/ai-innovation/copy.txt"
+        )
+
+        copy_text = response["Body"].read().decode("utf-8")
+
+        print(copy_text)
+
+        client = OpenAI(api_key=API_KEY)
+
+        result = client.images.generate(
+            model="gpt-image-1", prompt=copy_text, size="1x1024", quality="low"
+        )
+
+        image_b64 = result.data[0].b64_json
+        image = base64.b64decode(image_b64)
+
+        upload_path = os.path.join(os.getcwd(), "img.png")
+
+        with open(upload_path, "wb") as f:
+            f.write(image)
+
+        print("Success")
+        return image_b64
+
+    except Exception as e:
+        print(e)
+        return "fail!"
+
+    # image_path = os.path.join(upload_dir, "copy.txt")
+
+    # return "Success!"
+
+
+# @app.route('/')
+# def upload_aws():
+#     try:
+#         boto3.setup_default_session(profile_name="default")
+#         s3 = boto3.client("s3")
+
+#         s3.upload_file(
+#                     Body=pdf,
+#                     Bucket="vistar-dc",
+#                     Key=f"2025/09/ai-innovation/test/test.pdf",
+#                 )
+#     except ClientError as e:
+#         logging.error(e)
